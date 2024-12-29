a3ca93f51855ef4cb355c934fe27589e4d32aabe
Hasib Ahamed Sakib <hasibahamedsakib1@gmail.com>
2024-12-29 17:45:00 +0600

    final commit

--- src/components/Button/AddToCart/AddToCart.tsx
+++ src/components/Button/AddToCart/AddToCart.tsx
@@ -43,19 +43,22 @@ const AddToCart = ({
         </button>
       ) : (
         <div
-          className={`w-[150px] sm:w-[185px] h-8 mx-auto flex items-center justify-between gap-2  ${
+          className={`w-[150px] sm:w-[185px] h-8 mx-auto flex items-center justify-between gap-1 sm:gap-2  ${
             isModal
               ? "bg-success"
               : "bg-white/30 backdrop-blur-md hover:bg-success"
           } transition-all duration-300  py-2 rounded-md shadow text-sm group text-white `}
         >
-          <button className=" px-2 py-1 " onClick={handleRemoveFromCart}>
+          <button
+            className=" px-1 sm:px-2 py-1 "
+            onClick={handleRemoveFromCart}
+          >
             <RiDeleteBin6Line size={16} />
           </button>
           <span className="text-[12px] sm:text-sm">
             {productInCart ? `${productInCart.quantity} Added in Cart` : ""}
           </span>
-          <button className=" px-2 py-1 " onClick={handleAddToCart}>
+          <button className=" px-1 sm:px-2 py-1 " onClick={handleAddToCart}>
             <FiPlus size={16} />
           </button>
         </div>
